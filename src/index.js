8f794c9cc9bee808d259d869c0ebb5afa8423c0e
NicholasNeto <nicholasneto@outlook.com>
2021-11-09 22:34:02 -0300

    created endpoint for updated task to done

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body
   const { user } = request
   const { id } = request.params
-  
+
   const task = user.todos.find(task => task.id === id)
 
   task.title = title,
@@ -81,7 +81,11 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { id } = request.params
+  const task = user.todos.find(task => task.id === id)
+  task.done = true
+  return response.status(200).json(task)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
